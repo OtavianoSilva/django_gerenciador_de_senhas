15101fe436945c1c456000a0df73955995d939bb
OtavianoSilva <otavianosilva.escritor@gmail.com>
2023-11-11 10:53:14 -0300

    estilo homepage

--- project/urls.py
+++ project/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('core.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
